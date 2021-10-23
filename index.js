48c40d01d00fd60e91827064df972ee5cdd72708
Postman33 <lucky-pa777@yandex.ru>
2021-10-23 20:48:58 +0300

    Добавление визуализации ситуации на дороге

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYW50b256dXoiLCJhIjoiY2t2MmszNnlzMDEybjMwcHA1c3ZhcmQwYyJ9.0YoIoBX5rwFe18ix7TJKHw';
-// Центр Липецка
-//const Center = [39.06, 43.98]
-const Center = [39.557099, 52.605997]
-// Ход шага для случайных точек на КАРТЕ
-const deltaX = 0.01;
-const deltaY = 0.005;
-
-let count = 4;
-count = Math.sqrt(count);
+const Center = [39.557099, 52.605997] // Центр Липецка
+let count = 4; count = Math.sqrt(count);
 
 class Point {
-    constructor(lat0, long0) {
-        this.lat = lat0;
+    constructor(lat0,long0) {
+        this.lat  = lat0;
         this.long = long0;
     }
-
-    toString() {
+    toString(){
         return `lat=${this.lat}&lon=${this.long}`
+    }
+    get length(){
+        return Math.sqrt( this.lat*this.lat + this.long*this.long)
+    }
+    static Distance(P1, P2){
+        return (new Point(10000*(P2.lat-P1.lat),10000*(P2.long-P1.long))).length
+    }
+    get mapBoxFormat(){
+        return [this.lat,this.long]
     }
 }
 
+function StrToPoint(strPoint) {
+    let dateRegexp = /lat=(?<lat>[0-9]+\.[0-9]+)&lon=(?<lon>[0-9]+\.[0-9]+)/;
+    let groups = strPoint.match(dateRegexp).groups;
+    return new Point(+groups.lat, +groups.lon)
+}
 // Погода в точке
 let getCoordinates = async function (cb) {
     let response = await fetch("http://localhost:3000/calcPaths?date='2021-10-24'");
@@ -72,68 +78,68 @@ map.on('load', async () => {
 
     // Создаем маркеры на которых будем брать погоду
     await async function f() {
+        let min = 50000;
+        let max = 0;
         let response = await fetch("http://localhost:3000/getPoints?date='2021-10-25'");
         console.log(response.status);
         if (response.ok) {
             let json = await response.json();
             console.log(json)
+
+            for( let point in json){
+                if (!json.hasOwnProperty(point)) continue;
+                let pointPercMM = json[point];
+                if (pointPercMM > max) max = pointPercMM;
+                if (pointPercMM < min) min = pointPercMM;
+            }
+            for( let point in json){
+                if (!json.hasOwnProperty(point)) continue;
+                let R,G,B;
+                let pointClass = StrToPoint(point)
+                let pointPercMM = json[point];
+                let normalizeX = pointPercMM/max;
+                if (normalizeX>=0 && normalizeX <0.4){
+                   R = 0;
+                   G = 255;
+                   B = 0;
+                }
+                if (normalizeX>=0.4 && normalizeX <0.7){
+                    R = 255;
+                    G = 255;
+                    B = 0;
+                }
+                if (normalizeX>=0.7){
+                    R = 255;
+                    G = 0;
+                    B = 0;
+                }
+
+                polyGon(pointClass.mapBoxFormat, name = "polygon-"+point.toString(), rgbToHex(R, G, B))
+            }
+
+            console.log(`min=${min},max=${max}`)
         }
+
     }();
+
     getCoordinates(function (json) {
         let i = 1;
         for (let garage in json.garages) {
             addRoute(garage, [...json.garages[garage].res], rgbToHex(255, 51, i * 100))
             let ob = json.garages[garage]
             for (let c in ob.copy) {
-                let ob2 = ob.copy[c]
-                let mark = [ob2.coords.lat, ob2.coords.long];
-                new mapboxgl.Marker().setLngLat(mark).addTo(map);
+               // let ob2 = ob.copy[c]
+               // let mark = [ob2.coords.lat, ob2.coords.long];
+                //new mapboxgl.Marker().setLngLat(mark).addTo(map);
             }
             i++
         }
     })
 
 
-    map.addSource('route', {
-        'type': 'geojson',
-        'data': {
-            'type': 'Feature',
-            'properties': {},
-            'geometry': {
-                'type': 'LineString',
-
-                'coordinates': [
-                    [39.557099, 52.605997],
-                    [39.557451, 52.606005],
-                    [39.557541, 52.605965],
-                    [39.557824, 52.605754],
-                    [39.558843, 52.605179],
-                    [39.559862, 52.604713],
-                    [39.559008, 52.604067],
-                    [39.561514, 52.602921]
-
-                ]
-            }
-        }
-    });
-
-
     map.addSource('places', {
         'type': 'geojson',
         'data': places
-    });
-    map.addLayer({
-        'id': 'route',
-        'type': 'line',
-        'source': 'route',
-        'layout': {
-            'line-join': 'round',
-            'line-cap': 'round'
-        },
-        'paint': {
-            'line-color': '#888',
-            'line-width': 8
-        }
     });
 
     map.addLayer({
@@ -148,42 +154,31 @@ map.on('load', async () => {
             'icon-image': ['get', 'icon']
         }
     });
-    mark = [39.557099, 52.605997];
-    new mapboxgl.Marker().setLngLat(mark).addTo(map);
-    mark = [39.558843, 52.605179];
-    polyGon([39.558843, 52.605179], name = "polygon", rgbToHex(255, 0, 0))
-    new mapboxgl.Marker().setLngLat(mark).addTo(map);
 });
-//              [
-//                         [ 39.421459, 52.590104],
-//                         [ 39.604742, 52.512665],
-//                         [ 39.682811, 52.595398],
-//                         [ 39.601194, 52.656401],
-//                         [ 39.421459, 52.590104]
-//                     ]
+
 function polyGon(center, name = "polygon", HEX_Color) {
     let [lat, long] = center;
-    let dxy = 0.007
+    let dxy = 0.0047
+    let dy =0.002
     map.addSource(name, {
         'type': 'geojson',
         'data': {
             'type': 'Feature',
             'geometry': {
                 'type': 'Polygon',
-// These coordinates outline Maine.
                 'coordinates': [
                     [
-                        [lat - dxy, long + dxy / 1.2],
-                        [lat - dxy / 1.1, long],
-                        [lat + dxy, long],
-                        [lat + dxy, long + dxy / 1.4],
-                        [lat - dxy, long + dxy / 1.2],
+                        [lat - dxy, long + dxy -dy ],
+                        [lat - dxy , long-dy],
+                        [lat + dxy, long-dy],
+                        [lat + dxy, long + dxy-dy ],
+                        [lat - dxy, long + dxy-dy ],
                     ]
                 ]
             }
         }
     });
-// Add a new layer to visualize the polygon.
+    // Заливка полигона
     map.addLayer({
         'id': `${name}`,
         'type': 'fill',
@@ -194,15 +189,15 @@ function polyGon(center, name = "polygon", HEX_Color) {
             'fill-opacity': 0.5
         }
     });
-//Add a black outline around the polygon.
+
     map.addLayer({
-        'id': 'outline',
+        'id': `outline-${name}`,
         'type': 'line',
         'source': `${name}`,
         'layout': {},
         'paint': {
             'line-color': '#000',
-            'line-width': 3
+            'line-width': 1
         }
     });
 }
@@ -238,7 +233,7 @@ function addRoute(id, data, color = '#888') {
 }
 
 function componentToHex(c) {
-    var hex = c.toString(16);
+    let hex = c.toString(16);
     return hex.length === 1 ? "0" + hex : hex;
 }
 
